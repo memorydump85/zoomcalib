b67067e95c6c0baabcc9534845ace9c179500efa
Pradeep Ranganathan <rpradeep@umich.edu>
2015-07-30 15:14:35 -0400

    Fix precision problems in cython gram matrix: use float64(=double) instead of floats

--- gp/gram_matrix.pyx
+++ gp/gram_matrix.pyx
@@ -6,26 +6,28 @@ from libc.math cimport exp
 
 
 
+ctypedef np.float64_t float64_t
+
 @cython.boundscheck(False)
 @cython.wraparound(False)
 cpdef gram_matrix_sq_exp_1D(
-    np.ndarray[np.float_t, ndim=1] data,
-    float sigma_f,          # function/signal variance
-    float sigma_x,          # length-scale
-    float sigma_inv_noise   # sqrt(noise precision)
+    np.ndarray[float64_t, ndim=1] data,
+    float64_t sigma_f,          # function/signal variance
+    float64_t sigma_x,          # length-scale
+    float64_t sigma_inv_noise   # sqrt(noise precision)
     ):
 
     cdef int N
     N = data.shape[0]
 
-    cdef float noise_variance, x_precision
+    cdef float64_t noise_variance, x_precision
     noise_variance = 1./(sigma_inv_noise*sigma_inv_noise)
     x_precision = 1./(sigma_x*sigma_x)
 
     cdef int i, j
-    cdef float z
+    cdef float64_t z
 
-    cdef np.ndarray[np.float_t, ndim=2] K
+    cdef np.ndarray[float64_t, ndim=2] K
     K = np.empty((N, N))
 
     for i in xrange(N):
@@ -43,20 +45,20 @@ cpdef gram_matrix_sq_exp_1D(
 @cython.boundscheck(False)
 @cython.wraparound(False)
 cpdef gram_matrix_sq_exp_2D(
-    np.ndarray[np.float_t, ndim=2] data,
-    float sigma_f,          # Function/signal variance
-    float sigma_xx,         # 2D length-scale parameters
-    float sigma_yy,         #   ..
-    float corr_xy,          #   ..
-    float sigma_inv_noise   #   ..
+    np.ndarray[float64_t, ndim=2] data,
+    float64_t sigma_f,          # Function/signal variance
+    float64_t sigma_xx,         # 2D length-scale parameters
+    float64_t sigma_yy,         #   ..
+    float64_t corr_xy,          #   ..
+    float64_t sigma_inv_noise   #   ..
     ):
 
-    cdef np.ndarray[np.float_t, ndim=2] Sigma, Sigma_inv
+    cdef np.ndarray[float64_t, ndim=2] Sigma, Sigma_inv
     Sigma = np.array([ [ sigma_xx**2,  corr_xy    ],
                        [  corr_xy,    sigma_yy**2 ] ])
     Sigma_inv = np.linalg.inv(Sigma)
 
-    cdef float p, q, r, s
+    cdef float64_t p, q, r, s
     p = Sigma_inv[0,0]
     q = Sigma_inv[0,1]
     r = Sigma_inv[1,0]
@@ -65,13 +67,13 @@ cpdef gram_matrix_sq_exp_2D(
     cdef int N
     N = data.shape[0]
 
-    cdef float noise_variance
+    cdef float64_t noise_variance
     noise_variance = 1./(sigma_inv_noise*sigma_inv_noise)
 
     cdef int i, j
-    cdef float g, h, chi2, v
+    cdef float64_t g, h, chi2, v
 
-    cdef np.ndarray[np.float_t, ndim=2] K
+    cdef np.ndarray[float64_t, ndim=2] K
     K = np.empty((N, N))
 
     for i in xrange(N):
